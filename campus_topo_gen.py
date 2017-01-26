701c1e4b8ed4818bd167d93e6c424ec2da6a6cd1
Kyle Benson <kyle.edward.benson@gmail.com>
2017-01-25 16:54:07 -0800

    topology gen now adds server and building topology

--- campus_topo_gen.py
+++ campus_topo_gen.py
@@ -18,20 +18,22 @@ class CampusTopologyGenerator(object):
     Future versions may include additional redundant links, attributes such as
     latency or bandwidth, and shared risk groups."""
 
-    def __init__(self, core_size=4,
+    def __init__(self, core_size=4, servers=1, links_per_server=2,
                  percent_minor_buildings=0.15, # 10-15%
                  minor_buildings_per_distribution_router=8, # 7-8
                  links_per_distribution_router=2, links_per_building=2, nbuildings=200,
-                 add_building_topology=False,  # building topo is a tree, so no redundancy or clever routing to be done
+                 add_building_topology=True,  # building topo is a tree, so no redundancy or clever routing to be done
                  building_floors=5,  # estimate for larger buildings, should probably make a distribution
                  building_switches_per_floor=5,  # 4-6
-                 # TODO: hosts_per_floor_switch=???
-                 # inter_building_links=5-7
+                 hosts_per_floor_switch=10, # assuming just potentially relevant IoT devices.  should be much more...
+                 inter_building_links=6, # 5-7
                  # nsrlg_groups=4 # plus two sub-regions per group?
                  ):
         super(CampusTopologyGenerator, self).__init__()
 
         self.core_size = core_size
+        self.servers = servers
+        self.links_per_server = links_per_server
         self.percent_minor_buildings = percent_minor_buildings
         self.minor_buildings_per_distribution_router = minor_buildings_per_distribution_router
         self.links_per_distribution_router = links_per_distribution_router
@@ -40,20 +42,31 @@ class CampusTopologyGenerator(object):
         self.add_building_topology = add_building_topology
         self.building_floors = building_floors
         self.building_switches_per_floor = building_switches_per_floor
+        self.hosts_per_floor_switch = hosts_per_floor_switch
+        self.inter_building_links = inter_building_links
 
         self.topo = None
         self.core_nodes = None
         self.major_building_routers = None
         self.minor_building_routers = None
         self.distribution_routers = None
+        self.hosts = None
 
     def generate(self):
         """Generates and returns the topology."""
+
         # Start with core
         self.topo = nx.complete_graph(self.core_size)
         self.topo.graph['name'] = "Campus Network Topology"
         nx.relabel_nodes(self.topo, {i: "c%d" % i for i in range(self.core_size)}, copy=False)
         self.core_nodes = list(nx.nodes(self.topo))
+
+        # Server(s), e.g. data centers, are assumed to be located close to the core
+        for s in range(self.servers):
+            server_name = "s%d" % s
+            self.topo.add_node(server_name)
+            for server_router in random.sample(self.core_nodes, self.links_per_server):
+                self.topo.add_edge(server_router, server_name)
 
         # Add buildings
         nminor_buildings = int(math.ceil(self.nbuildings * self.percent_minor_buildings))
@@ -105,9 +118,30 @@ class CampusTopologyGenerator(object):
 
         # Optionally add in-building topologies
         if self.add_building_topology:
-            raise NotImplementedError("Currently don't support in-building topologies")
+            self.hosts = []
+            for b in self.major_building_routers:
+                self.create_building_topology(b)
+
+        # TODO: add inter-building links
 
         return self.topo
+
+    def create_building_topology(self, building):
+        """Creates a depth 3 tree topology of switches as a building topology."""
+        # CONSIDER: give each host a unique host #
+        for floor in range(self.building_floors):
+            floor_switch_name = "f%d-%s" % (floor, building)
+            self.topo.add_node(floor_switch_name)
+            self.topo.add_edge(building, floor_switch_name)
+            for rack_switch in range(self.building_switches_per_floor):
+                rack_switch_name = "r%d-%s" % (rack_switch, floor_switch_name)
+                self.topo.add_node(rack_switch_name)
+                self.topo.add_edge(floor_switch_name, rack_switch_name)
+                for host in range(self.hosts_per_floor_switch):
+                    host_name = "h%d-%s" % (host, rack_switch_name)
+                    self.topo.add_node(host_name)
+                    self.topo.add_edge(rack_switch_name, host_name)
+                    self.hosts.append(host_name)
 
     def get(self):
         if self.topo is not None:
@@ -119,8 +153,9 @@ class CampusTopologyGenerator(object):
         """Draw the graph using matplotlib in a color-coordinated manner."""
         try:
             import matplotlib.pyplot as plt
-            print 'Node colors: red=core, blue=major-building, green=distribution, yellow=minor-building'
-            colormap = {'c': 'r', 'b': 'b', 'd': 'g', 'm': 'y'}
+            print 'Node colors: red=core, blue=major-building, green=distribution, yellow=minor-building, cyan=server, magenta=host, black=floor-switch, white=rack-switch'
+            # TODO: ignore building internals?
+            colormap = {'c': 'r', 'b': 'b', 'd': 'g', 'm': 'y', 's': 'c', 'h': 'm', 'f': 'k', 'r': 'w'}
             node_colors = [colormap[node[0]] for node in self.topo.nodes()]
             nx.draw(self.topo, node_color=node_colors)
             plt.show()
@@ -135,8 +170,17 @@ class CampusTopologyGenerator(object):
 
 
 if __name__ == '__main__':
-    t = CampusTopologyGenerator(nbuildings=8)
+    test_run = True
+
+    if test_run:
+        # build smaller topology for visualizing
+        t = CampusTopologyGenerator(nbuildings=8, hosts_per_floor_switch=2,
+                                    building_floors=2, building_switches_per_floor=2)
+    else:
+        t = CampusTopologyGenerator()
+
     g = t.get()
     print nx.info(g)
-    t.draw()
+    if test_run:
+        t.draw()
     t.write()
