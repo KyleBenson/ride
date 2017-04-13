8b25fcf4d4214fab6c7029dc5493704acb2c6002
Kyle Benson <kyle.edward.benson@gmail.com>
2017-04-13 13:51:44 -0700

    ignoring all cleanup command output

--- mnrun.sh
+++ mnrun.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# start everything fresh
-tools/onos_reset.sh
-sudo mn -c > /dev/null
-mkdir logs 2> /dev/null
-rm logs/* 2> /dev/null
+echo "Resetting ONOS, Mininet, and log files to start fresh..." 
+tools/onos_reset.sh > /dev/null 2>&1
+sudo mn -c > /dev/null 2>&1
+mkdir logs > /dev/null 2>&1
+rm logs/* > /dev/null 2>&1
 
 # use first non-loopback interface as IP address of SDN Controller (we run it locally)
 CONTROLLER_IP=`hostname -I | awk '{print $1}'`
 echo "SDN controller at $CONTROLLER_IP"
 sudo ./mininet_smart_campus_experiment.py --ip "$CONTROLLER_IP" $@
 
-sudo mn -c > /dev/null
+sudo mn -c > /dev/null 2>&1
 
 # iperf seems to hang due to mininet not closing properly 
-sudo pkill iperf
+sudo pkill iperf > /dev/null 2>&1
