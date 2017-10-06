000ba30828892b95411dccced086b0dcf6eb7be3
Kyle Benson <kyle.edward.benson@gmail.com>
2017-10-06 14:20:09 -0700

    giving preference to locally-installed onos command

--- tools/onos_reset.sh
+++ tools/onos_reset.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-if [ -d ~/repos/onos ]; then
-    # when we have built ONOS from source and used e.g. onos-install to install/run it
-    pushd ~/repos/onos
-    ONOS_CMD="onos localhost"
-else
+if [ -d /opt/onos/ ]; then
     # when we've downloaded/extracted the ONOS tarball and just configured it as a systemctl service
     ONOS_CMD=/opt/onos/bin/onos
+elif [ -d ~/repos/onos ]; then
+    # when we have built ONOS from source and used e.g. onos-install to install/run it
+    # we'll assume that we have aliases setup...
+    #pushd ~/repos/onos
+    ONOS_CMD="onos localhost"
+else
+    echo "ERROR: no onos in /opt/onos or ~/repos/onos: where's your 'onos' command???"
+    exit -1
 fi
 
 # TODO: check if we're root (if [ `whoami` == root ]) and run su -c $CMD $ONOS_USER
@@ -14,7 +18,3 @@ FINAL_CMD="$ONOS_CMD $RESET_CMD"
 
 # echo "$FINAL_CMD"
 $FINAL_CMD
-
-if [ -d ~/repos/onos ]; then
-    popd
-fi
