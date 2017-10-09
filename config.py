a9b428463ed06f02f2f39ebe703e2befbcdacc61
Kyle Benson <kyle.edward.benson@gmail.com>
2017-10-09 10:42:37 -0700

    refactored mac/ip-assigning helper functions into config.py so we can use them with stats

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 ####      CONFIGURATIONS
 # Configurations that you might need to change for a specific installation.
-# TODO: this file can be sourced in bash to collect these variables too!  BUT we need to do string formatting differently...
 
 # can just change this and not the others if everything is running under same user account...
 DEFAULT_USER='vagrant'
@@ -106,6 +105,71 @@ IP_SUBNET = '10.128.0.0/%d' % HOST_IP_N_MASK_BITS
 NAT_SERVER_IP_ADDRESS = '11.0.0.%d/24'
 MULTICAST_ADDRESS_BASE = u'224.0.0.1'  # must be unicode!
 
+
+import re
+from topology_manager.test_sdn_topology import mac_for_host
+
+
+def get_ip_mac_for_host(host):
+    # See note in mininet_smart_campus_experiment.setup_topology about host format
+    # XXX: differentiate between regular hosts and server hosts
+    if '-' in host:
+        host_num, building_type, building_num = re.match('h(\d+)-([mb])(\d+)', host).groups()
+    else:  # must be a server
+        building_type, host_num = re.match('h?([xs])(\d+)', host).groups()
+        building_num = 0
+
+    # Assign a number according to the type of router this host is attached to
+    if building_type == 'b':
+        router_code = 131
+        router_mac_code = 'bb'
+    elif building_type == 'm':
+        router_code = 144
+        router_mac_code = 'aa'
+    # cloud
+    elif building_type == 'x':
+        router_code = 199
+        router_mac_code = 'cc'
+    # edge server
+    elif building_type == 's':
+        router_code = 255
+        router_mac_code = '55'
+    else:
+        raise ValueError("unrecognized building type '%s' so cannot assign host IP address!" % building_type)
+    _ip = "10.%d.%s.%s/%d" % (router_code, building_num, host_num, HOST_IP_N_MASK_BITS)
+    _mac = mac_for_host(int(host_num))
+    _mac = "00:%s:%s%s" % (router_mac_code, str(building_num).rjust(2, '0'), _mac[8:])
+    # XXX: onos expects upper case mac addresses
+    return _ip, _mac.upper()
+
+
+def get_mac_for_switch(switch, is_cloud=False, is_server=False):
+    # BUGFIX: need to manually specify the mac to set DPID properly or Mininet
+    # will just use the number at the end of the name, causing overlaps.
+    # HACK: slice off the single letter at start of name, which we assume it has;
+    # then convert the number to a MAC.
+    mac = mac_for_host(int(switch[1:]))
+    # Disambiguate one switch type from another by setting the first letter
+    # to be a unique one corresponding to switch type and add in the other 0's.
+    # XXX: if the first letter is outside those available in hexadecimal, assign one that is
+    first_letter = switch[0]
+    if first_letter == 'm':
+        first_letter = 'a'
+    elif first_letter == 'g':
+        first_letter = 'e'
+    # We'll just label rack/floor switches the same way; we don't actually even use them currently...
+    elif first_letter == 'r':
+        first_letter = 'f'
+
+    # XXX: we're out of letters! need to assign a second letter for the cloud/server switches...
+    second_letter = '0'
+    if is_cloud:
+        second_letter = 'c'
+    elif is_server:
+        second_letter = 'e'
+
+    mac = first_letter + second_letter + ':00:00:' + mac[3:]
+    return str(mac).lower()
 
 #########       EXPERIMENT    CONFIG    ##########
 
--- mininet_smart_campus_experiment.py
+++ mininet_smart_campus_experiment.py
@@ -7,7 +7,6 @@ import errno
 import logging
 import os
 import random
-import re
 from subprocess import Popen
 
 import topology_manager
@@ -29,7 +28,6 @@ from mininet.cli import CLI
 from mininet.link import TCLink
 
 from topology_manager.networkx_sdn_topology import NetworkxSdnTopology
-from topology_manager.test_sdn_topology import mac_for_host  # used for manual MAC assignment
 
 from config import *
 from ride.config import *
@@ -221,76 +219,16 @@ class MininetSmartCampusExperiment(SmartCampusExperiment):
         # import the switches, hosts, and server(s) from our specified file
         self.topo = NetworkxSdnTopology(self.topology_filename)
 
-        def __get_mac_for_switch(switch, is_cloud=False, is_server=False):
-            # BUGFIX: need to manually specify the mac to set DPID properly or Mininet
-            # will just use the number at the end of the name, causing overlaps.
-            # HACK: slice off the single letter at start of name, which we assume it has;
-            # then convert the number to a MAC.
-            mac = mac_for_host(int(switch[1:]))
-            # Disambiguate one switch type from another by setting the first letter
-            # to be a unique one corresponding to switch type and add in the other 0's.
-            # XXX: if the first letter is outside those available in hexadecimal, assign one that is
-            first_letter = switch[0]
-            if first_letter == 'm':
-                first_letter = 'a'
-            elif first_letter == 'g':
-                first_letter = 'e'
-            # We'll just label rack/floor switches the same way; we don't actually even use them currently...
-            elif first_letter == 'r':
-                first_letter = 'f'
-
-            # XXX: we're out of letters! need to assign a second letter for the cloud/server switches...
-            second_letter = '0'
-            if is_cloud:
-                second_letter = 'c'
-            elif is_server:
-                second_letter = 'e'
-
-            mac = first_letter + second_letter + ':00:00:' + mac[3:]
-            return str(mac).lower()
-
         for switch in self.topo.get_switches():
-            mac = __get_mac_for_switch(switch)
+            mac = get_mac_for_switch(switch)
             s = self.net.addSwitch(switch, dpid=mac, cls=OVSKernelSwitch)
             log.debug("adding switch %s at DPID %s" % (switch, s.dpid))
             self.switches.append(s)
             if self.topo.is_cloud_gateway(switch):
                 self.cloud_gateways.append(s)
 
-        def __get_ip_mac_for_host(host):
-            # See note in docstring about host format
-            # XXX: differentiate between regular hosts and server hosts
-            if '-' in host:
-                host_num, building_type, building_num = re.match('h(\d+)-([mb])(\d+)', host).groups()
-            else:  # must be a server
-                building_type, host_num = re.match('h?([xs])(\d+)', host).groups()
-                building_num = 0
-
-            # Assign a number according to the type of router this host is attached to
-            if building_type == 'b':
-                router_code = 131
-                router_mac_code = 'bb'
-            elif building_type == 'm':
-                router_code = 144
-                router_mac_code = 'aa'
-            # cloud
-            elif building_type == 'x':
-                router_code = 199
-                router_mac_code = 'cc'
-            # edge server
-            elif building_type == 's':
-                router_code = 255
-                router_mac_code = '55'
-            else:
-                raise ValueError("unrecognized building type '%s' so cannot assign host IP address!" % building_type)
-            _ip = "10.%d.%s.%s/%d" % (router_code, building_num, host_num, HOST_IP_N_MASK_BITS)
-            _mac = mac_for_host(int(host_num))
-            _mac = "00:%s:%s%s" % (router_mac_code, str(building_num).rjust(2, '0'), _mac[8:])
-            # XXX: onos expects upper case mac addresses
-            return _ip, _mac.upper()
-
         for host in self.topo.get_hosts():
-            _ip, _mac = __get_ip_mac_for_host(host)
+            _ip, _mac = get_ip_mac_for_host(host)
             h = self.net.addHost(host, ip=_ip, mac=_mac)
             self.hosts.append(h)
 
@@ -299,11 +237,11 @@ class MininetSmartCampusExperiment(SmartCampusExperiment):
             # difficult to work with multiple interfaces on a host (e.g. ONOS can only handle
             # a single MAC address per host).
             server_switch_name = server.replace('s', 'e')
-            server_switch_dpid = __get_mac_for_switch(server_switch_name, is_server=True)
+            server_switch_dpid = get_mac_for_switch(server_switch_name, is_server=True)
             # Keep server name for switch so that the proper links will be added later.
             self.server_switch = self.net.addSwitch(server, dpid=server_switch_dpid, cls=OVSKernelSwitch)
             host = 'h' + server
-            _ip, _mac = __get_ip_mac_for_host(host)
+            _ip, _mac = get_ip_mac_for_host(host)
             s = self.net.addHost(host, ip=_ip, mac=_mac)
             # ENHANCE: handle multiple servers
             self.server = s
@@ -314,13 +252,13 @@ class MininetSmartCampusExperiment(SmartCampusExperiment):
             if self.with_cloud:
                 # HACK: Same hack with adding local server
                 cloud_switch_name = cloud.replace('x', 'f')
-                cloud_switch_dpid = __get_mac_for_switch(cloud_switch_name, is_cloud=True)
+                cloud_switch_dpid = get_mac_for_switch(cloud_switch_name, is_cloud=True)
                 # Keep server name for switch so that the proper links will be added later.
                 cloud_switch = self.net.addSwitch(cloud, dpid=cloud_switch_dpid, cls=OVSKernelSwitch)
                 self.cloud_switches.append(cloud_switch)
                 # ENHANCE: handle multiple clouds
                 host = 'h' + cloud
-                _ip, _mac = __get_ip_mac_for_host(host)
+                _ip, _mac = get_ip_mac_for_host(host)
                 self.cloud = self.net.addHost(host, ip=_ip, mac=_mac)
                 self.net.addLink(cloud_switch, self.cloud)
             # otherwise just add a host to prevent topology errors
