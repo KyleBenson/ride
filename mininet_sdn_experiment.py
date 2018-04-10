b824877618028265d8eb65c45e3da42f5ae95f8f
Kyle Benson <kyle.edward.benson@gmail.com>
2018-04-09 17:32:55 -0700

    BUGFIX: mininet_sdn_exp wasn't saving its experiment_type

--- mininet_sdn_experiment.py
+++ mininet_sdn_experiment.py
@@ -79,6 +79,7 @@ class MininetSdnExperiment(NetworkExperiment):
 
         self.experiment_duration = experiment_duration
         self.record_parameter('exp_duration', experiment_duration)
+        self.record_parameter('experiment_type', 'mininet')
         # We'll optionally drop to a CLI after the experiment completes for further poking around
         self.show_cli = show_cli
 
